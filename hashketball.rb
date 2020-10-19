7d80468b597d8c24985cbdef0e3a14c4b5f0860f
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-19 15:54:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,8 +187,7 @@ def big_shoe_rebounds
       if player[:shoe] > biggest_shoes 
         biggest_shoes = player[:shoe] 
       end 
-      return biggest_shoes
     end
-    
+    return biggest_shoes
   end 
 end 
